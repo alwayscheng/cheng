7398803fd4b09d7c957eef58b35365d596248a21
alwayscheng <50835657+alwayscheng@users.noreply.github.com>
2019-07-04 21:03:47 +0800

    Add files via upload

---
+++ LED/LED.ino
@@ -0,0 +1,34 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+}
+
+int income=0;
+
+void loop()
+{
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+   digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  if(Serial.available()>0){
+    income=Serial.read()-'0';
+    if ((income & 0x1) == 1 )
+      digitalWrite(2, HIGH);
+    if ((income>>1 & 0x1) == 1 )
+      digitalWrite(3, HIGH);
+    if ((income>>2 & 0x1) == 1 )
+      digitalWrite(4, HIGH);
+    if ((income>>3 & 0x1) == 1 )
+      digitalWrite(5, HIGH);
+    digitalWrite(6,HIGH) ;
+    delay(1000) ;
+    
+  }
+ 
+ 
+}
